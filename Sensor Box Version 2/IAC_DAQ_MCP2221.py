11854e8f624d3f6e46b4ce9741240741c60a3301
MidasOG <midasdeen@gmail.com>
2023-05-08 14:30:03 +0200

    f

--- Sensor Box Version 2/IAC_DAQ_MCP2221.py
+++ Sensor Box Version 2/IAC_DAQ_MCP2221.py
@@ -10,6 +10,12 @@ import time
 import adafruit_vl53l0x
 from cedargrove_nau7802 import NAU7802
 
+
+kgperr = 0
+testload = 0
+r0 = 0
+load = 0
+
 # Load cell
 loadCelSensor = NAU7802(board.I2C(), address=0x2a, active_channels=1)
 
@@ -26,8 +32,19 @@ try:
         loadCellValue = loadCelSensor.read()
         tofValue = tofSensor.range
 
+        if callibrated == False:
+            if input("press 1 when no load is applied") == 1:
+                r0 = loadCellValue# sensor value when no load is applied
+                testload = input("Apply load to sensor and enter how much (N).")
+                kgperr = testload / loadCelSensor.read() # the loading in kg that one lil sensor value is worth
+                callibrated = True 
+           
+     
+        load = 9.81(kgperr*loadCelSensor.read() - r0) # in newton
+        
+
         # Output sensor data
-        print("Load cell: {:.0f}, Distance: {:.0f}".format(loadCellValue, tofValue))
+        print("Load cell: {:.0f}, Distance: {:.0f}".format(load, tofValue))
         # print("Load cell: {:.0f}".format(loadCellValue))
 
         # Sleep
